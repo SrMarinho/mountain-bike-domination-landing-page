ab443505499c07cab484f55255c42c49b400b31b
Matheus Marinho <temarinho76@gmail.com>
2025-08-04 10:25:03 -0300

    add: components

--- src/libs/threejs/entities/graphic_component.ts
+++ src/libs/threejs/entities/graphic_component.ts
@@ -1,14 +1,41 @@
-import * as THREE from 'three'
+import { Object3D } from 'three'
 import type { ComponentInterface } from '../interfaces/component_interface'
+import type { TransformationComponent } from './transformation_component'
 
 class GraphicComponent implements ComponentInterface {
-  constructor(mesh: THREE.Object3D) {}
-  update(deltaTime: number): void {
-    throw new Error('Method not implemented.')
-  }
+  private object3D: Object3D
+  private transform: TransformationComponent | null
 
+  constructor(object3D: Object3D) {
+    this.object3D = object3D
+    this.transform = null
+  }
   dispose(): void {
     throw new Error('Method not implemented.')
   }
+
+  public setTransformationComponent(transform: TransformationComponent): void {
+    this.transform = transform
+    this.syncWithTransform()
+  }
+
+  public update(deltaTime: number): void {
+    if (this.transform) {
+      this.syncWithTransform()
+    }
+  }
+
+  private syncWithTransform(): void {
+    if (!this.transform) return
+
+    this.object3D.position.copy(this.transform.position)
+    this.object3D.quaternion.copy(this.transform.rotation)
+    this.object3D.scale.copy(this.transform.scale)
+  }
+
+  public getObject3D(): Object3D {
+    return this.object3D
+  }
 }
-}
+
+export { GraphicComponent }
---
+++ src/libs/threejs/entities/physic_component.ts
@@ -0,0 +1,32 @@
+// physic_component.ts
+import { Vector3 } from 'three'
+import type { ComponentInterface } from '../interfaces/component_interface'
+import { TransformationComponent } from './transformation_component'
+
+class PhysicComponent implements ComponentInterface {
+  private velocity: Vector3
+  private mass: number
+  private transform: TransformationComponent | null
+
+  constructor(mass: number = 1) {
+    this.velocity = new Vector3()
+    this.mass = mass
+    this.transform = null
+  }
+  dispose(): void {
+    throw new Error('Method not implemented.')
+  }
+
+  public setTransformationComponent(transform: TransformationComponent): void {
+    this.transform = transform
+  }
+
+  public update(deltaTime: number): void {
+    if (!this.transform) return
+
+    // Atualiza posição
+    this.transform.position.add(this.velocity.clone().multiplyScalar(deltaTime))
+  }
+}
+
+export { PhysicComponent }
